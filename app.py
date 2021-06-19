a18965036e9739eae20ce57bfffca7256aaf5b76
eukestad <69314608+eukestad@users.noreply.github.com>
2021-06-18 21:10:28 -0500

    stripped down version

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #Import necessary libraries
 from flask import Flask, render_template, Response, jsonify, request, abort
-from werkzeug.utils import secure_filename
-import imghdr
+# from werkzeug.utils import secure_filename
+# import imghdr
 from get_data import prediction, get_sel_images
 # from get_data import livePrediction
 import os
@@ -11,33 +11,33 @@ import os
 app = Flask(__name__)
 
 
-# ---------------------prediction function---------------------------# 
-def predicted_image(img_file):
-    #print("img_file", img_file)
-    experiment_images = ["people1.jpg", "people2.jpg", "people3.jpg", "people4.jpg", "people5.jpg", "people6.jpg", "people7.jpg"]
-    if img_file not in experiment_images:
-        base_path = "./static/upload/"
-    else:
-        base_path = "./Resources/Experiment/"
-    img_path = base_path + img_file
-    print("path:", img_path)
-    data = prediction(img_path)
-    return data
-# -------------------------------------------------------------------#
+# # ---------------------prediction function---------------------------# 
+# def predicted_image(img_file):
+#     #print("img_file", img_file)
+#     experiment_images = ["people1.jpg", "people2.jpg", "people3.jpg", "people4.jpg", "people5.jpg", "people6.jpg", "people7.jpg"]
+#     if img_file not in experiment_images:
+#         base_path = "./static/upload/"
+#     else:
+#         base_path = "./Resources/Experiment/"
+#     img_path = base_path + img_file
+#     print("path:", img_path)
+#     data = prediction(img_path)
+#     return data
+# # -------------------------------------------------------------------#
 
 
-UPLOAD_FOLDER = './static/upload'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
-app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png']
+# UPLOAD_FOLDER = './static/upload'
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+# app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png']
 
-def validate_image(stream):
-    header = stream.read(512)
-    stream.seek(0) 
-    format = imghdr.what(None, header)
-    if not format:
-        return None
-    return '.' + (format if format != 'jpeg' else 'jpg')
+# def validate_image(stream):
+#     header = stream.read(512)
+#     stream.seek(0) 
+#     format = imghdr.what(None, header)
+#     if not format:
+#         return None
+#     return '.' + (format if format != 'jpeg' else 'jpg')
    
 
 # default app route
@@ -46,41 +46,41 @@ def index():
     return render_template('index.html')
 
 
-#app route for uploading
-@app.route('/upload', methods=["GET", "POST"])
-def uploader():
-   if request.method == 'POST':
-      f = request.files['file']
-      filename = secure_filename(f.filename)
-      if filename != '':
-          file_ext = os.path.splitext(filename)[1]
-          if file_ext not in app.config['UPLOAD_EXTENSIONS'] or file_ext != validate_image(f.stream):
-              abort(400)
-          f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-          print("Yay! It Worked!")
-          return '', 204
+# #app route for uploading
+# @app.route('/upload', methods=["GET", "POST"])
+# def uploader():
+#    if request.method == 'POST':
+#       f = request.files['file']
+#       filename = secure_filename(f.filename)
+#       if filename != '':
+#           file_ext = os.path.splitext(filename)[1]
+#           if file_ext not in app.config['UPLOAD_EXTENSIONS'] or file_ext != validate_image(f.stream):
+#               abort(400)
+#           f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#           print("Yay! It Worked!")
+#           return '', 204
 
 
-# app route for image prediction
-@app.route("/get_image/<img>", methods=["GET", "POST"])
-def get_predicted_image(img):
-    print("image file rendered:",img)
-    #img = "people5.jpg"
-    data = predicted_image(img)   
-    print(data)
-    return jsonify(data)
+# # app route for image prediction
+# @app.route("/get_image/<img>", methods=["GET", "POST"])
+# def get_predicted_image(img):
+#     print("image file rendered:",img)
+#     #img = "people5.jpg"
+#     data = predicted_image(img)   
+#     print(data)
+#     return jsonify(data)
 
-# app route for image file selection
-@app.route("/api/v1.0/select_option", methods=["GET", "POST"])
-def get_selected_images():    
-    img_files = get_sel_images()
-    return jsonify(img_files)
+# # app route for image file selection
+# @app.route("/api/v1.0/select_option", methods=["GET", "POST"])
+# def get_selected_images():    
+#     img_files = get_sel_images()
+#     return jsonify(img_files)
 
 
-# # app route for video feed
-# @app.route("/video_feed", methods=["GET", "POST"])
-# def video_feed():    
-#     return Response(livePrediction(), mimetype='multipart/x-mixed-replace; boundary=frame')
+# app route for video feed
+@app.route("/video_feed", methods=["GET", "POST"])
+def video_feed():    
+    return Response(livePrediction(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == "__main__":
--- get_data.py
+++ get_data.py
@@ -72,54 +72,54 @@ def prediction(path):
 
     return image_data
 
-# def livePrediction():
-#     print("Streaming started")
-#     video_capture = cv2.VideoCapture(0)
+def livePrediction():
+    print("Streaming started")
+    video_capture = cv2.VideoCapture(0)
 
-#     mask_label = {0:'Mask Found',1:'No Mask Found'}
-#     color_label = {0:(0,255,0),1:(0,0,255)}
-#     # loop over frames from the video file stream
-#     while True:
-#         # grab the frame from the threaded video stream
-#         success, frame = video_capture.read()
+    mask_label = {0:'Mask Found',1:'No Mask Found'}
+    color_label = {0:(0,255,0),1:(0,0,255)}
+    # loop over frames from the video file stream
+    while True:
+        # grab the frame from the threaded video stream
+        success, frame = video_capture.read()
 
-#         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#         faces = face_model.detectMultiScale(gray,
-#                                             scaleFactor=1.1,
-#                                             minNeighbors=5,minSize=(60, 60),
-#                                          flags=cv2.CASCADE_SCALE_IMAGE)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_model.detectMultiScale(gray,
+                                            scaleFactor=1.1,
+                                            minNeighbors=5,minSize=(60, 60),
+                                         flags=cv2.CASCADE_SCALE_IMAGE)
     
-#         # convert the input frame from BGR to RGB 
-#         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # convert the input frame from BGR to RGB 
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-#         #check for no. of faces in the image
-#         # if more than one face found, check if they are social distancing (extra code)
-#         if len(faces)>=1:
-#             label = [0 for i in range(len(faces))]
+        #check for no. of faces in the image
+        # if more than one face found, check if they are social distancing (extra code)
+        if len(faces)>=1:
+            label = [0 for i in range(len(faces))]
             
-#             # For every face found,   
-#             for i in range(len(faces)):
-#                 (x,y,w,h) = faces[i]
-#                 crop = rgb[y:y+h,x:x+w]
-#                 crop = cv2.resize(crop,(128,128))
-#                 crop = np.reshape(crop,[1,128,128,3])/255.0        
-#                 mask_result = model.predict(crop)
-#                 #print(color_label[round(mask_result[0][0])])
+            # For every face found,   
+            for i in range(len(faces)):
+                (x,y,w,h) = faces[i]
+                crop = rgb[y:y+h,x:x+w]
+                crop = cv2.resize(crop,(128,128))
+                crop = np.reshape(crop,[1,128,128,3])/255.0        
+                mask_result = model.predict(crop)
+                #print(color_label[round(mask_result[0][0])])
 
-#                 cv2.putText(frame,mask_label[round(mask_result[0][0])],(x, y-10), cv2.FONT_HERSHEY_SIMPLEX,1,color_label[round(mask_result[0][0])],2)
-#                 cv2.rectangle(frame,(x,y),(x+w,y+h), color_label[round(mask_result[0][0])],3)
+                cv2.putText(frame,mask_label[round(mask_result[0][0])],(x, y-10), cv2.FONT_HERSHEY_SIMPLEX,1,color_label[round(mask_result[0][0])],2)
+                cv2.rectangle(frame,(x,y),(x+w,y+h), color_label[round(mask_result[0][0])],3)
 
-#         ret, buffer = cv2.imencode('.jpg', frame)
-#         frame = buffer.tobytes()
-#         yield (b'--frame\r\n'
-#                 b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
-#         key = cv2.waitKey(10)
+        key = cv2.waitKey(10)
     
-#         if key == 27: 
-#             break
-#     video_capture.release()
-#     cv2.destroyAllWindows()
+        if key == 27: 
+            break
+    video_capture.release()
+    cv2.destroyAllWindows()
 
 # Function to load images from the folder
 def load_images_from_folder(folder):
